249c4946394538e973a858ac1eb224b53d26bae6
Hack Reactor Students <santiago.archila@gmail.com>
2013-11-23 12:35:19 -0800

    added dragging of main player

--- collider.js
+++ collider.js
@@ -11,6 +11,7 @@ var enemies = d3.range(0,100).map(function(i)
     });
 console.log(enemies);
 var board = d3.select("svg");
+
 enemiesDrawn= board.selectAll('circle').data(enemies).enter()
     .append('circle')
       .attr('class', 'enemy')
@@ -18,6 +19,28 @@ enemiesDrawn= board.selectAll('circle').data(enemies).enter()
       .attr('cy', function (enemy) {return enemy.y;})
       .attr('style', function(enemy){return "fill:rgb(" + enemy.rgb.join(",") + ")"})
       .attr('r', 10);
+var el = d3.select('ellipse');
+var playerData = {
+  x: Math.random()*800 + 10,
+  y: Math.random()*600 + 10
+};
+
+var dragMove = function (){
+  movePlayer(el,d3.event.dx, d3.event.dy);
+};
+
+var movePlayer = function(player,x,y){
+  playerData.x += x;
+  playerData.y += y;
+  player.data([playerData])
+    .attr('cx', function (d) {return d.x;})
+    .attr('cy', function (d) {return d.y;});
+};
+
+var drag = d3.behavior.drag().on('drag', dragMove);
+
+movePlayer(el,0,0);
+el.call(drag);
 
 var updateEnemies = function(enemies){
   enemies.map(function(enemy){
@@ -26,7 +49,6 @@ var updateEnemies = function(enemies){
     enemy.rgb = [Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255)];
   });
 };
-
 
 var moveEnemies = function (){
   updateEnemies(enemies);
